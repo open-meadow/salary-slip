81a29c1b70df20a834207d236ce9bc01640c5f4b
open-meadow <rohan.varughese@protonmail.com>
2023-06-29 22:06:23 -0400

    Display calculation

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -1,48 +1,58 @@
 const recieveValues = () => {
-    let gs = document.querySelector('#gross-salary').value;
-    let bonus = document.querySelector('#bonus').value;
-    let allowance = document.querySelector('#allowance').value;
-    let incomeTax = document.querySelector('#income-tax').value;
-    let empIns = document.querySelector('#emp-ins').value;
-    let cpp = document.querySelector('#cpp').value;
-    let gender = document.querySelector('input[name="gender"]:checked').value;
-    let dependants = document.querySelector('#dependants').value;
+  let gs = Number(document.querySelector("#gross-salary").value);
+  let bonus = Number(document.querySelector("#bonus").value);
+  let allowance = Number(document.querySelector("#allowance").value);
+  let incomeTax = Number(document.querySelector("#income-tax").value);
+  let empIns = Number(document.querySelector("#emp-ins").value);
+  let cpp = Number(document.querySelector("#cpp").value);
+  let gender = document.querySelector('input[name="gender"]:checked').value;
+  let dependants = Number(document.querySelector("#dependants").value);
 
-    calculate(gs, bonus, allowance, incomeTax, empIns, cpp, gender, dependants);
-}
+  calculate(gs, bonus, allowance, incomeTax, empIns, cpp, gender, dependants);
+};
 
-const calculate = (gs, bonus, allowance, incomeTax, empIns, cpp, gender, dependants) => {
-    let additions = bonus + allowance;
+const calculate = (
+  gs,
+  bonus,
+  allowance,
+  incomeTax,
+  empIns,
+  cpp,
+  gender,
+  dependants
+) => {
+  let additions = bonus + allowance;
 
-    // calculate deductions
-    let empInsVal = (empIns / 100) * gs;
-    let cppVal = (cpp / 100) * gs;
-    let incomeTaxVal;
+  // calculate deductions
+  let empInsVal = (empIns / 100) * gs;
+  let cppVal = (cpp / 100) * gs;
+  let incomeTaxVal;
 
-    switch (dependants) {
-        case 3:
-            incomeTaxVal = ((incomeTax / 100) * gs) - ((2 / 100) * gs);
-            break;
-        case 4:
-            incomeTaxVal = ((incomeTax / 100) * gs) - ((4 / 100) * gs);
-            break;
-        default:
-            incomeTaxVal = ((incomeTax / 100) * gs);
-            break;
-    }
+  switch (dependants) {
+    case 3:
+      incomeTaxVal = ((incomeTax / 100) * gs) - ((2 / 100) * gs);
+      break;
+    case 4:
+      incomeTaxVal = ((incomeTax / 100) * gs) - ((4 / 100) * gs);
+      break;
+    default:
+      incomeTaxVal = (incomeTax / 100) * gs;
+      break;
+  }
 
-    if (gender === "Female") {
-        incomeTaxVal = ((incomeTax / 100) * gs) - ((2 / 100) * gs);
-    }
+  if (gender === "Woman") {
+    incomeTaxVal = (incomeTax / 100) * gs - (2 / 100) * gs;
+  }
 
-    let deductions = empInsVal + cppVal + incomeTaxVal;
+  let deductions = empInsVal + cppVal + incomeTaxVal;
+  let takeHomeSalary = (gs + additions) - deductions;
 
-    let takeHomeSalary = (gs + additions) - deductions;
-
-    displey(gs, additions, deductions, takeHomeSalary);
-}
+  display(gs, additions, deductions, takeHomeSalary);
+};
 
 const display = (gs, additions, deductions, takeHomeSalary) => {
-
-    document.getElementById("gs-answer").innerHTML = "Gross Salary: $" + gs;
-}
+  document.getElementById("gs-answer").innerHTML = "Gross Salary: $" + gs;
+  document.getElementById("additions-answer").innerHTML = "Additions: $" + additions;
+  document.getElementById("deductions-answer").innerHTML = "Deductions: $" + deductions;
+  document.getElementById("takeHomeSalary-answer").innerHTML = "Take Home Salary: $" + takeHomeSalary;
+};
